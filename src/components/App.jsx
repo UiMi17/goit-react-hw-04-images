c4e6fd9246104672a4f965f7f954fe714cf58dd9
Dan Dudaiev <roffrooff@gmail.com>
2023-04-24 20:10:08 +0300

    hooks upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
@@ -8,127 +8,95 @@ import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class App extends Component {
-  state = {
-    images: [],
-    per_page: 12,
-    currentPage: 1,
-    query: '',
-    isLoadMorePresent: false,
-    loading: false,
-    isModalShown: false,
-    currentModalImg: {
-      largeImageURL: '',
-      tags: '',
-    },
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [per_page] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [isLoadMorePresent, setIsLoadMorePresent] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isModalShown, setIsModalShown] = useState();
+  const [currentModalImg, setCurrentModalImg] = useState({
+    largeImageURL: '',
+    tags: '',
+  });
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.currentPage !== this.state.currentPage
-    ) {
-      this.handleStateUpdate();
-    }
-  }
+  useEffect(() => {
+    handleStateUpdate();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query, currentPage]);
 
-  getImages = async () => {
+  const getImages = async () => {
     try {
       const API_KEY = '34819242-61fdcfe42d1461d5acd80d71b';
-      const perPage = this.state.per_page;
-      const currentPage = this.state.currentPage;
-      const query = this.state.query;
 
-      this.setState({ loading: true });
+      setLoading(true);
       const response = await axios.get(
-        `https://pixabay.com/api/?q=${query}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+        `https://pixabay.com/api/?q=${query}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`
       );
       return response;
     } catch (error) {
       toast.error(error.message);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  onSubmit = ev => {
+  const onSubmit = ev => {
     ev.preventDefault();
 
     const inputValue = ev.currentTarget.elements.search.value;
-    this.setState({ query: inputValue, images: [], currentPage: 1 });
+
+    setQuery(inputValue);
+    setImages([]);
+    setCurrentPage(1);
   };
 
-  handleLoadMoreBtnClick = () => {
-    this.setState(prevState => {
-      return {
-        currentPage: prevState.currentPage + 1,
-      };
-    });
+  const handleLoadMoreBtnClick = () => {
+    setCurrentPage(currentPage + 1);
   };
 
-  handleModalOpen = (largeImageURL, tags) => {
-    this.setState({
-      currentModalImg: { largeImageURL, tags },
-      isModalShown: true,
-    });
+  const handleModalOpen = (largeImageURL, tags) => {
+    setCurrentModalImg({ largeImageURL, tags });
+    setIsModalShown(true);
   };
 
-  handleModalClose = ev => {
+  const handleModalClose = ev => {
     if (ev.code === 'Escape' || ev.target === ev.currentTarget) {
-      this.setState({ isModalShown: false });
+      setIsModalShown(false);
     }
   };
 
-  handleStateUpdate = async () => {
-    const fetchedImages = await this.getImages();
+  const handleStateUpdate = async () => {
+    const fetchedImages = await getImages();
 
-    this.setState(
-      prevState => {
-        return {
-          images: [...prevState.images, ...fetchedImages.data.hits],
-        };
-      },
-      () => {
-        if (fetchedImages.data.hits.length === 0) {
-          this.setState({ isLoadMorePresent: false });
-          toast.warning("Sorry, there's no images found!");
-        } else if (
-          fetchedImages.data.hits.length < this.state.per_page ||
-          fetchedImages.data.totalHits <= this.state.per_page
-        ) {
-          this.setState({ isLoadMorePresent: false });
-          toast.warning("You've reached the end of search results!");
-        } else {
-          this.setState({ isLoadMorePresent: true });
-        }
-      }
-    );
+    setImages([...images, ...fetchedImages.data.hits]);
+    if (fetchedImages.data.hits.length === 0) {
+      setIsLoadMorePresent(false);
+      toast.warning("Sorry, there's no images found!");
+    } else if (
+      fetchedImages.data.hits.length < per_page ||
+      fetchedImages.data.totalHits <= per_page
+    ) {
+      setIsLoadMorePresent(false);
+      toast.warning("You've reached the end of search results!");
+    } else {
+      setIsLoadMorePresent(true);
+    }
   };
 
-  render() {
-    const {
-      images,
-      loading,
-      isLoadMorePresent,
-      isModalShown,
-      currentModalImg,
-    } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.onSubmit} />;
-        <ImageGallery images={images} handleModalOpen={this.handleModalOpen} />
-        {loading && <Loader />}
-        {!loading && isLoadMorePresent && (
-          <Button handleLoadMoreBtnClick={this.handleLoadMoreBtnClick} />
-        )}
-        {isModalShown && (
-          <Modal
-            image={currentModalImg}
-            handleModalClose={this.handleModalClose}
-          />
-        )}
-        <ToastContainer />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />;
+      <ImageGallery images={images} handleModalOpen={handleModalOpen} />
+      {loading && <Loader />}
+      {!loading && isLoadMorePresent && (
+        <Button handleLoadMoreBtnClick={handleLoadMoreBtnClick} />
+      )}
+      {isModalShown && (
+        <Modal image={currentModalImg} handleModalClose={handleModalClose} />
+      )}
+      <ToastContainer />
+    </>
+  );
+};
